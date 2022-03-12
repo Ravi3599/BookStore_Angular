addf6ac9db543872a823d4ff6d075464bc665cf3
Ravi3599 <ravikantdukre2018@gmail.com>
2022-03-12 08:30:23 +0530

    ADDED: created book store angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './MyComponents/user/user.component';
+import { BookComponent } from './MyComponents/book/book.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    BookComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
